f10ba0b324d7f08bcdfcc6c327757203e6fbc71e
Smit Vachhani <svachhan@svachhan-mn1.linkedin.biz>
2021-04-08 12:00:45 -0700

    added css practices

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import Navbar from './Navbar';
-import TradeModal from './TradeModal';
-import '../assets/css/App.css';
+import React from "react";
+import Navbar from "./Navbar";
+import TradeModal from "./TradeModal";
+import "../assets/css/App.css";
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
       isConnected: false,
-    }
+    };
     this.connectCache = this.connectCache.bind(this);
   }
 
@@ -19,7 +19,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="opyn-main__container">
-        <Navbar connect={this.connectCache}/>
+        <Navbar connect={this.connectCache} />
         <TradeModal connected={this.state.isConnected} />
       </div>
     );
